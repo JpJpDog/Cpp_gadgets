77d30cc318488b6d7d403e0e6915194c3a5f1175
shenyushi <shenyushi@sjtu.edu.cn>
2021-05-31 14:47:42 +0800

    fix bug

--- aaa.cc
+++ aaa.cc
@@ -29,6 +29,7 @@ static inline u16 encode_one(char ch) {
   }
 }
 
+// 已有一段seq的 `key` ，和下一个字符 `ch` ，把下一个key求出（向后挪动一格）
 static inline u16 encode_forward(int key, char ch) {
   key <<= 2;
   key += encode_one(ch);
@@ -50,12 +51,14 @@ static inline void add_map(SegMap &seg_map, u16 key, int loc) {
     seg_map[key] = {loc};
   } else {
     int last_loc = *iter->second.rbegin();
+    // 避免两个segment重叠在一起
     if (loc >= last_loc + kSegLen) {
       iter->second.push_back(loc);
     }
   }
 }
 
+// 遍历每个segment，求出key，放到segmap中，然后找到最多的那个。
 static std::tuple<u16, std::vector<int>> find_most_freq(char *seq, int len) {
   SegMap seg_map;
   u16 key = encode(seq);
@@ -64,26 +67,29 @@ static std::tuple<u16, std::vector<int>> find_most_freq(char *seq, int len) {
     key = encode_forward(key, seq[i + kSegLen - 1]);
     add_map(seg_map, key, i);
   }
+  // 选最大的segment
   auto pair = std::max_element(seg_map.begin(), seg_map.end(),
                                [](const SegPair &p1, const SegPair &p2) {
                                  return p1.second.size() < p2.second.size();
-                               });
-  return std::make_tuple(pair->first, std::move(pair->second));
+                               });  // 这里用了lamda表达式表示怎么判断最大
+  return std::make_tuple(
+      pair->first, std::move(pair->second));  // 用move移动，减少复制的开销
 }
 
+// 根据 `result` ，把 `seq` 的segment中去掉。在原地改变，并改变长度。
 static void remove_repeat(char *seq, int *len, CompResult &result) {
   auto &locs = std::get<1>(result);
   if (locs.empty()) return;
-  int p_loc = -kSegLen;
+  int prev_loc = -kSegLen;
   char *p = seq, *q = seq;
   for (int loc : locs) {
-    int cp_len = loc - p_loc - kSegLen;
+    int cp_len = loc - prev_loc - kSegLen;
     memcpy(q, p, cp_len);
     q += cp_len;
     p += cp_len + kSegLen;
-    p_loc = loc;
+    prev_loc = loc;
   }
-  memcpy(q, p, *len - p_loc - kSegLen);
+  memcpy(q, p, *len - prev_loc - kSegLen);
   *len -= locs.size() * kSegLen;
 }
 
@@ -100,7 +106,8 @@ std::vector<CompResult> remove_repeat_n(char *seq, int *len) {
   return result;
 }
 
-// 下面是解压缩还原的过程，仅用来测试压缩过程。可以供写那一部分的同学参考
+// 下面是解压缩还原的过程，仅用来测试压缩过程。可以供写那一部分的同学参考。
+// 从标准输入读seq，压缩，再解压，输出segment和解压结果（没有读写文件）
 #define DEBUG
 
 #ifdef DEBUG
@@ -142,17 +149,18 @@ void fake_decompress(int *len, std::vector<CompResult> &results) {
     std::tie(key, locs) = std::move(result);
     fake_decode_seg(key, seg);
     char *p = buf1, *q = buf2;
-    int p_loc = -kSegLen;
+    int prev_loc = -kSegLen;
     for (int loc : locs) {
-      int cp_len = loc - p_loc - kSegLen;
+      int cp_len = loc - prev_loc - kSegLen;
       memcpy(q, p, cp_len);
       q += cp_len;
       p += cp_len;
       memcpy(q, seg, kSegLen);
       q += kSegLen;
-      p_loc = loc;
+      prev_loc = loc;
     }
-    memcpy(q, p, *len - p_loc - kSegLen);
+    memcpy(q, p, *len - prev_loc);
+    // printf("%s\n", buf2);
     *len += locs.size() * kSegLen;
     std::swap(buf1, buf2);
   }
@@ -186,4 +194,3 @@ int main() {
   printf("%s\n", buf11);
 }
 #endif
-
